47645f8abb1e1830ccaf3c6c70003108f88add70
TomasJ1993 <tomastomasj@gmail.com>
2020-08-22 15:04:25 +0300

    Text change in 'about me' section

--- src/components/AboutPage/Info.js
+++ src/components/AboutPage/Info.js
@@ -1,46 +1,44 @@
-import React from 'react'
-import Title from '../Title'
-import aboutBcg from '../../images/office.jpg'
-import styled from 'styled-components'
+import React from "react";
+import Title from "../Title";
+import aboutBcg from "../../images/office.jpg";
+import styled from "styled-components";
 
-
-export default function Info({bodyColor}) {
-
-    return <InfoWrapper bodyColor={bodyColor}>
-        <section data-aos='fade-up'>
-            <div className="container">
-            <div className="row">
-                <div className="col-10 mx-auto col-md-6 my-3"><img src={aboutBcg} 
-                className="img-fluid img-thumbnail" 
+export default function Info({ bodyColor }) {
+  return (
+    <InfoWrapper bodyColor={bodyColor}>
+      <section data-aos="fade-up">
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 my-3">
+              <img
+                src={aboutBcg}
+                className="img-fluid img-thumbnail"
                 alt="about image"
-                style={{background: 'var(--darkGrey)'}}
-                /></div>
-                <div className="col-10 mx-auto col-md-6 my-3">
-                    <Title title="about me" titleDescription="know everything you need to know">
-                    </Title>
-                    <p className="text-lead text-muted my-3">
-                    I am front-end developer
-                    currently working for Overhand
-                    Fitness, a start-up company
-                    specializing in building and
-                    running a system that provides
-                    an exciting gamified fitness
-                    experience. As a programmer, I
-                    have a goal to become an expert
-                    in both front-end and backend
-                    development. Therefore, my
-                    ambition is to work for a
-                    company that pursues the same
-                    direction - full-stack web
-                    development. I specialize in ReactJS, Angular, Android/Java.
-                    </p>
-                </div>
+                style={{ background: "var(--darkGrey)" }}
+              />
             </div>
+            <div className="col-10 mx-auto col-md-6 my-3">
+              <Title
+                title="about me"
+                titleDescription="know everything you need to know"
+              ></Title>
+              <p className="text-lead text-muted my-3">
+                I am front-end developer currently working for Udentity, a
+                start-up company specializing in building an education
+                technology platform that connects students in Asia with overseas
+                universities. As a programmer, I have a goal to become an expert
+                in both front-end and backend development. Therefore, my
+                ambition is to work for a company that pursues the same
+                direction - full-stack web development.
+              </p>
             </div>
-        </section>
+          </div>
+        </div>
+      </section>
     </InfoWrapper>
+  );
 }
 
 const InfoWrapper = styled.div`
-background: ${props=>(props.bodyColor)};
-`
+  background: ${(props) => props.bodyColor};
+`;
